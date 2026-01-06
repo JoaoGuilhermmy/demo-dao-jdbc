6660853df292813f9d8e72c0b3c4ac26fb703e69
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-01-06 20:43:48 -0300

    FindById implemetation

--- src/application/App.java
+++ src/application/App.java
@@ -1,16 +1,17 @@
 package application;
 
-import java.util.Date;
 import java.util.Locale;
-
-import model.entities.Sellere;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
-        Sellere department = new Sellere(2, "Books");
-        Seller seller = new Seller(1, "Luan", "luan@gmail.com", new Date(), 3.000, department);
+
+        SellerDao sellerDao = DaoFactory.creatSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         IO.println(seller);
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,5 +1,82 @@
 package model.dao.impl;
 
-public class SellerDaoJDBC {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import db.DB;
+import db.DbException;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    };
+
+    @Override
+    public void insert(Seller dep) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void update(Seller dep) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void delete(Seller dep) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn
+                    .prepareStatement(
+                            "SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+                                    + "ON seller.DepartmentId = department.Id " + "Where seller.Id = ?");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+
+            if (rs.next()) {
+                Department dep = new Department();
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("DepName"));
+                Seller seller = new Seller();
+                seller.setBaseSalary(rs.getDouble("BaseSalary"));
+                seller.setBirthDate(rs.getDate("BirthDate"));
+                seller.setDepartment(dep);
+                seller.setEmail(rs.getString("Email"));
+                seller.setId(rs.getInt("Id"));
+                seller.setName(rs.getString("Name"));
+                return seller;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
 
 }
