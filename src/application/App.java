d06d84d272e7a3d5b29df59500ccb932a0e518bb
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-01-07 09:21:29 -0300

    findByDepartment implementation

--- src/application/App.java
+++ src/application/App.java
@@ -1,19 +1,27 @@
 package application;
 
+import java.util.List;
 import java.util.Locale;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
-
         SellerDao sellerDao = DaoFactory.creatSellerDao();
 
+        IO.println("==== TEST 1: seller findbyid ====");
         Seller seller = sellerDao.findById(1);
-
         IO.println(seller);
+
+        IO.println("\n==== TEST 2: seller findByDepartment ====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller s : list) {
+            IO.println(s);
+        }
 
     }
 }
